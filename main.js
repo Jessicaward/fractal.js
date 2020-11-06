8086895ac9e6c5ce70a158239d18219f513b0418
Jessica Ward <Jessicawardiom@gmail.com>
2020-11-06 21:06:58 +0000

    This is the coolest, yet laggiest piece of software I've ever written...

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 window.onload = function(){
     var window = {
-        width: 200,
-        height: 200
+        width: 100,
+        height: 100
     };
     var mouse = {
         x: 0,
@@ -11,6 +11,7 @@ window.onload = function(){
     var canvas = document.querySelector("canvas");
     var ctx = canvas.getContext("2d");
     var maximumNumberOfRecursiveIterations = 64;
+    var zoom = 1;
 
     canvas.width = window.width;
     canvas.height = window.height;
@@ -25,16 +26,16 @@ window.onload = function(){
     //A complex number is a number that can be expressed as a point (so a+b(i))
     //This takes the X and Y coordinates for a pixel, divides them to get the percentage of the overall...
     //width and height of the window, and returns the percentage on a point from -1 to +1.
-    let pixelToPoint = (x,y) => math.complex((x / window.width) * 2 - 1, 1 - (y / window.height) * 2).add(pan);
+    let pixelToPoint = (x,y) => math.complex((x / window.width) * 2 - 1, 1 - (y / window.height) * 2).div(zoom).add(pan);
 
     //pointToColour simply represents a point as a colour, wavy dude.
     function pointToColour(point) {
         var percentage = julia(point) / maximumNumberOfRecursiveIterations;
-        return `rgb(${percentage * 255}, ${percentage * 255}, ${percentage * 255})`;
+        return `rgb(${point.re * 64}, ${point.im * 64}, ${percentage * 255})`;
     }
 
     function update() {
-        console.log(constant.toString());
+        console.log(constant.toString() + " at " + zoom + "X");
         draw();
     }
 
@@ -49,6 +50,8 @@ window.onload = function(){
         mouse.y = event.clientY - canvas.offsetTop;
 
         pan = pixelToPoint(mouse.x, mouse.y);
+
+        zoom *= 2;
 
         update();
     }
@@ -73,6 +76,7 @@ window.onload = function(){
     //Recursive function that applies simple equation:
     //f(z) = z^2 + c
     function julia(z, i = 0) {
+        console.log("julia");
         z = z.mul(z);
         z = z.add(constant);
 
